1b817047ef1fe2c6a82ae14558d8474d7961069c
Ad <95490130+AlexDevlpr@users.noreply.github.com>
2022-07-24 14:01:22 +0100

    Update normal_ver.lua

--- normal_ver.lua
+++ normal_ver.lua
@@ -1,7 +1,7 @@
 getgenv().logger_stat = false
 
 local library = loadstring(game:HttpGet("https://pastebin.com/raw/jLe6HkTP"))()
-local Window = library:AddWindow("Roblox Input Logger by Ad", {
+local Window = library:AddWindow("Roblox Input Logger by Ad - V1.1", {
 		main_color = Color3.fromRGB(41, 74, 122),
 		min_size = Vector2.new(500, 140),
 		toggle_key = Enum.KeyCode.RightShift,
@@ -17,25 +17,17 @@ local Window = library:AddWindow("Roblox Input Logger by Ad", {
 	
 	Tab:AddButton("Clear Console", function()
         rconsoleclear()
-        rconsolename("Roblox Input Logger by Ad")
+        rconsolename("Roblox Input Logger by Ad - V1.1")
         p("Idle.")
 		end)
 	
 	Tab:Show()
 	library:FormatWindows() --Idek what this does lmafo
 
-function greencolor() --because yes
-   local exec = identifyexecutor()
-   if string.find(exec, "Synapse") then return "@@GREEN@@"
-   elseif string.find(exec, "ScriptWare") then return "green"
-   elseif exec == "Krnl" then return "@@GREEN@@" end
-end
-
-function p(s) rconsoleprint(greencolor()) rconsoleprint("\n[CONSOLE]: "..s) end -- i was lazy so i made a function
-
+function p(a,b)if string.find(identifyexecutor(),"Script")then if b==true then rconsoleprint("\n[CONSOLE]: ","white")rconsoleprint(tostring(a),"green")else rconsoleprint("\n[CONSOLE]: ","white")rconsoleprint(tostring(a),"red")end elseif string.find(identifyexecutor(),"Synapse")then if b==true then rconsoleprint("@@LIGHT_GRAY@@")rconsoleprint("\n[CONSOLE]: ")rconsoleprint("@@GREEN@@")rconsoleprint(tostring(a))elseif b==false then rconsoleprint("@@LIGHT_GRAY@@")rconsoleprint("\n[CONSOLE]: ")rconsoleprint("@@RED@@")rconsoleprint(tostring(a))end elseif rconsoleprint then if b==true then rconsoleprint("@@LIGHT_GRAY@@")rconsoleprint("\n[CONSOLE]: ")rconsoleprint("@@GREEN@@")rconsoleprint(tostring(a))elseif b==false then rconsoleprint("@@LIGHT_GRAY@@")rconsoleprint("\n[CONSOLE]: ")rconsoleprint("@@RED@@")rconsoleprint(tostring(a))end end end
 -- < Main Script > --
 
-rconsolename("Roblox Input Logger by Ad#9999")
+rconsolename("Roblox Input Logger by Ad#9999 - V1.1")
 p("Idle.")
 
 game:GetService("UserInputService").InputBegan:Connect(function(iobj, gpe)
